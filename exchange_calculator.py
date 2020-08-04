4991f3661e0d6c045e27ec314ea9f2604610dea4
Orlando Ramirez <ramirez.orlando17@gmail.com>
2020-08-04 17:33:36 -0400

    changed the input text, now you know that you are working with bolivars

--- exchange_calculator.py
+++ exchange_calculator.py
@@ -1,4 +1,4 @@
-venezuelan_bolivars = float(input('How many Colombian pesos do you have? '))
+venezuelan_bolivars = float(input('How many bolivars do you have? '))
 dollar_value = 266749.46
 dollars = venezuelan_bolivars/dollar_value
 print(f'You have {dollars:.2f} dollars')
