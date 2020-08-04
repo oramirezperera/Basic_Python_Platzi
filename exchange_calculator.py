78f219c91ed8893259f3dcc59f219574d74b92e2
Orlando Ramirez <ramirez.orlando17@gmail.com>
2020-08-04 17:21:49 -0400

    changes to the program, now only shows 2 decimals

--- exchange_calculator.py
+++ exchange_calculator.py
@@ -1,4 +1,4 @@
 colombian_pesos = float(input('How many Colombian pesos do you have? '))
 dollar_value = 3875
 dollars = colombian_pesos/dollar_value
-print(f'You have {dollars} dollars')
+print(f'You have {dollars:.2f} dollars')
