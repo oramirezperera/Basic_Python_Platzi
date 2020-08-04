d577525a5b46b8e23a8b0807a5a8066be404285b
Orlando Ramirez <ramirez.orlando17@gmail.com>
2020-08-04 17:31:52 -0400

    Changes to the Exchange calculator now works with venezuelan bolivars

--- exchange_calculator.py
+++ exchange_calculator.py
@@ -1,4 +1,4 @@
-colombian_pesos = float(input('How many Colombian pesos do you have? '))
-dollar_value = 3875
-dollars = colombian_pesos/dollar_value
+venezuelan_bolivars = float(input('How many Colombian pesos do you have? '))
+dollar_value = 266749.46
+dollars = venezuelan_bolivars/dollar_value
 print(f'You have {dollars:.2f} dollars')
