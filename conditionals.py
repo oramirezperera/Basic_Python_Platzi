5f55c49d2d6b4551dcc34140a4c4e1c1a9db4107
Orlando Ramirez <ramirez.orlando17@gmail.com>
2020-08-05 11:45:19 -0400

    Commented all the old code, now works with elif too

--- conditionals.py
+++ conditionals.py
@@ -1,7 +1,15 @@
-age = int(input('How old are you? '))
+# age = int(input('How old are you? '))
 
-if age >= 18:
-    print('you have age majority')
+# if age >= 18:
+#     print('you have age majority')
+# else:
+#     print('you don\'t have age majority :( ')
+
+number = int(input('write a number '))
+
+if number > 5:
+    print('The number is greater than 5')
+elif number == 5:
+    print('The number is equal to 5')
 else:
-    print('you don\'t have age majority :( ')
-
+    print('The number is lesser than 5')
