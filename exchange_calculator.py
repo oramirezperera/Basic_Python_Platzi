b502e62a6deddc9a373c86d96bdaf6b50306ba2a
Orlando Ramirez <ramirez.orlando17@gmail.com>
2020-08-05 12:23:18 -0400

    now you can enter 3 different exchanges to calculate

--- exchange_calculator.py
+++ exchange_calculator.py
@@ -1,4 +1,28 @@
-colombian_pesos = float(input('How many Colombian pesos do you have? '))
-dollar_value = 3875
-dollars = colombian_pesos/dollar_value
-print(f'You have {dollars:.2f} dollars')
+menu = """
+Welcome to the exchange calculator 😎💲💱
+
+1- Colombian pesos
+2- Argenitinian pesos
+3- Mexican pesos
+
+choose your option
+"""
+option = input(menu)
+
+if option == '1':
+    colombian_pesos = float(input('How many Colombian pesos do you have? '))
+    dollar_value = 3875
+    dollars = colombian_pesos/dollar_value
+    print(f'You have {dollars:.2f} dollars')
+elif option == '2':
+    argentinian_pesos = float(input('How many Argentinian pesos do you have? '))
+    dollar_value = 65
+    dollars = argentinian_pesos/dollar_value
+    print(f'You have {dollars:.2f} dollars')
+elif option == '3':
+    mexican_pesos = float(input('How many Mexican pesos do you have? '))
+    dollar_value = 24
+    dollars = mexican_pesos/dollar_value
+    print(f'You have {dollars:.2f} dollars')
+else:
+    print('please choose a right option')
