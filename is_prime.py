38866feae8e4292335219435567ef4fb80c3b2cc
Orlando Ramirez <ramirez.orlando17@gmail.com>
2020-08-07 20:05:26 -0400

    created the is_prime file, and created the basic structure, creating the is_prime function

---
+++ is_prime.py
@@ -0,0 +1,13 @@
+def is_prime():
+    
+
+def run():
+    number = int(input('Write a number please: '))
+    if is_prime(number):
+        print('is prime')
+    else:
+        print('is not prime')
+
+
+if __name__ == '__main__':
+    run()
