c3507c56bc34e869fc3d8517079188550b1002b2
Orlando Ramirez <ramirez.orlando17@gmail.com>
2020-08-07 20:16:49 -0400

    created a is prime function, now if you enter a number the program tells you if it\'s prime or not

--- is_prime.py
+++ is_prime.py
@@ -1,5 +1,15 @@
-def is_prime():
-    
+def is_prime(number):
+    counter = 0
+    for i in range (1, number + 1): #Runs all the numbers between 2 and the input number
+        if i == 1 or i == number: # skips the number 1 and the input number
+            continue
+        if number % i == 0: #cheks the module 
+            counter += 1
+    if counter == 0:
+        return True
+    else:
+        return False
+
 
 def run():
     number = int(input('Write a number please: '))
