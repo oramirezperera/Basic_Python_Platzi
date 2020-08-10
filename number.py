7defc124f8e8c0a1556a31c273cdb8f6d61b78ef
Orlando Ramirez <ramirez.orlando17@gmail.com>
2020-08-10 09:48:46 -0400

    created the game

--- number.py
+++ number.py
@@ -2,6 +2,14 @@ import random
 
 def run():
     aleatory_number = random.randint(1, 100)
+    chosen_number = int(input('Choose a number between 1 and 100: '))
+    while chosen_number != aleatory_number:
+        if chosen_number < aleatory_number:
+            print('The number is bigger')
+        else:
+            print('The number is smaller')
+        chosen_number = int(input('Choose another number'))
+    print('You won!')
 
 if __name__ == '__main__':
     run()
