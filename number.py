2f161878eec8d404a6a176274a46ca92abf87c37
Orlando Ramirez <ramirez.orlando17@gmail.com>
2020-08-10 09:53:15 -0400

    fixed some text to better looking, added a step counter

--- number.py
+++ number.py
@@ -3,13 +3,15 @@ import random
 def run():
     aleatory_number = random.randint(1, 100)
     chosen_number = int(input('Choose a number between 1 and 100: '))
+    steps = 0
     while chosen_number != aleatory_number:
         if chosen_number < aleatory_number:
             print('The number is bigger')
         else:
             print('The number is smaller')
-        chosen_number = int(input('Choose another number'))
-    print('You won!')
+        chosen_number = int(input('Choose another number: '))
+        steps += 1
+    print(f'You won! in {steps} steps')
 
 if __name__ == '__main__':
     run()
