36f7411e758deaa4dd099fa1008930ce201c72cd
Orlando Ramirez <ramirez.orlando17@gmail.com>
2020-08-10 12:26:21 -0400

    password generator is finished, gives you a password from random numbers, symbols, and capital and lower letters

--- password_generator.py
+++ password_generator.py
@@ -1,5 +1,25 @@
+import random
+
+def password_generator():
+    upper_case = ['A', 'B', 'C', 'D', 'F', 'G']
+    lower_case = ['a', 'b', 'c', 'd', 'f', 'g']
+    symbols = ['!', '#', '$', '&', '/', '(', ')']
+    numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    
+    char = upper_case + lower_case + symbols + numbers
+    password = []
+
+    for i in range(15):
+        char_random = random.choice(char)
+        password.append(char_random)
+    
+    password = ''.join(password)
+    return password
+
 def run():
-    pass
+    password = password_generator()
+    print(f'Your new password is {password}')
+
 
 if __name__ == '__main__':
     run()
