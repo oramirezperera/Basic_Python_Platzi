26501c52cf6bb0d03df567d0ee18215cc9bdcf8b
Orlando Ramirez <ramirez.orlando17@gmail.com>
2020-08-10 12:33:40 -0400

    added some new lower case, upper case, and symbols, now it's going to be more robust the password generated

--- password_generator.py
+++ password_generator.py
@@ -1,9 +1,9 @@
 import random
 
 def password_generator():
-    upper_case = ['A', 'B', 'C', 'D', 'F', 'G']
-    lower_case = ['a', 'b', 'c', 'd', 'f', 'g']
-    symbols = ['!', '#', '$', '&', '/', '(', ')']
+    upper_case = ['A', 'B', 'C', 'D', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    lower_case = ['a', 'b', 'c', 'd', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    symbols = ['!', '#', '$', '&', '/', '(', ')', '.']
     numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
     
     char = upper_case + lower_case + symbols + numbers
